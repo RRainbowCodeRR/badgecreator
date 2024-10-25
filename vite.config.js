25072f56f32c6a4e52d043e1677947790e19ea25
RRainbowCodeRR <gimblynimbly@gmail.com>
2024-10-24 19:18:58 -0500

    Deploy to GitHub Pages

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  base: "/qr-badge-creator/", // Replace with your repository name
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-  },
-});
